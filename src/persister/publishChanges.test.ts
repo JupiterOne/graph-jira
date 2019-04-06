b7f0edb1936180242fa22fbd59a6ae3606b85e06
erkangz <erkang.zheng@lifeomic.com>
2019-04-06 09:33:51 -0400

    Add project url and webLink

--- src/persister/publishChanges.test.ts
+++ src/persister/publishChanges.test.ts
@@ -51,6 +51,7 @@ describe("Convert data after fetching", () => {
       self: "https://dualboot-test.atlassian.net/rest/api/3/project/10000",
       simplified: false,
       style: "classic",
+      webLink: "https://dualboot-test.atlassian.net/browse/FP",
     },
     {
       _class: "Project",
@@ -65,6 +66,7 @@ describe("Convert data after fetching", () => {
       self: "https://dualboot-test.atlassian.net/rest/api/3/project/10001",
       simplified: false,
       style: "classic",
+      webLink: "https://dualboot-test.atlassian.net/browse/SP",
     },
   ];
 
