60998f9f4c813af47aa34cae37b2e1cf59ab9fde
poornima-metron <poornima@metronlabs.com>
2024-07-11 15:33:45 +0530

    updated steps

--- src/steps/issues.ts
+++ src/steps/issues.ts
@@ -259,10 +259,10 @@ export const issueSteps: IntegrationStep<IntegrationConfig>[] = [
         resourceName: 'Jira Issue',
         _type: ISSUE_ENTITY_TYPE,
         _class: ISSUE_ENTITY_CLASS,
-        // partial: true,
-        // indexMetadata: {
-        //   enabled: false,
-        // },
+        partial: true,
+        indexMetadata: {
+          enabled: false,
+        },
       },
     ],
     relationships: [
